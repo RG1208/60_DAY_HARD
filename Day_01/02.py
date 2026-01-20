7bfb7591eb844ef7b9567078a9326b53a3ead2e7
Rachit Garg <rachitgarg1208@gmail.com>
2026-01-21 00:39:44 +0530

    feat: Day 1 Python Basics

---
+++ Day_01/02.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+def add():
+    return a+b
+
+print(add())
---
+++ Day_01/03.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a: "))
+
+def checker():
+    if a/2 == 0 :
+        print("Even")
+    else:
+        print("Odd")
+
+checker()
